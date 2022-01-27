d4b673adeaad900bbb166dbd15ae2a9bbac4bea1
Alireza Rahimi <rahimialireza.az@gmail.com>
2022-01-27 13:10:10 +0330

    change export method based on server database

--- src/ServerBased.py
+++ src/ServerBased.py
@@ -1,50 +1,52 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_excel("test.xlsx")
+df = pd.read_excel("lesion_injury_report.xlsx")
 areas = [f"{i}" for i in range(13)]
-new_limp_column = 'لنگش جدید'
-cow_number_column = "شماره دام"
-day_column = "روز"
-month_column = "ماه"
-year_column = "سال"
+new_limp_column = 'reference_cause_new_limp'
+leg_area_number = "leg_area_number"
+finger_number = "finger_number"
+cow_number_column = "cow_id"
+day_column = "day"
+month_column = "month"
+year_column = "year"
 
 
 def get_felemon():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['0'].isna()]
-    felemon = temp[[cow_number_column, day_column, month_column, year_column, "0"]].groupby(
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number] == 0]
+    felemon = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).nunique()
     return felemon.sum()[0]
 
 
 def get_dramatit():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['10'].isna()]
-    dramatit = temp[[cow_number_column, day_column, month_column, year_column, "10"]].groupby(
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number] == 10]
+    dramatit = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).nunique()
     return dramatit.sum()[0]
 
 
 def get_kaf_som():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['4'].isna()]
-    kaf_som = temp[[cow_number_column, day_column, month_column, year_column, "4"]].groupby(
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number] == 4]
+    kaf_som = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).nunique()
     return kaf_som.sum()[0]
 
 
 def get_white_line():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['2'].isna() | ~temp['3'].isna() | ~temp['4'].isna()]
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number].isin([2, 3, 4])]
 
     def white_line_count(temp_df):
-        two = temp_df["2"].values
-        three = temp_df["3"].values
+        two = temp_df.loc[temp_df[leg_area_number] == 2, finger_number].values
+        three = temp_df.loc[temp_df[leg_area_number] == 3, finger_number].values
         values = np.unique(np.concatenate([two, three]))
         values = values[~np.isnan(values)]
         if values.shape[0] != 0:
-            four = temp_df["4"].values
+            four = temp_df.loc[temp_df[leg_area_number] == 4, finger_number].values
             four = four[~np.isnan(four)]
             if four.shape[0] != 0:
                 return values.shape[0] - np.intersect1d(four, values).shape[0]
@@ -53,23 +55,24 @@ def get_white_line():
         else:
             return 0
 
-    white_line = temp[[cow_number_column, day_column, month_column, year_column, "2", "3", "4"]].groupby(
+    white_line = temp[
+        [cow_number_column, day_column, month_column, year_column, leg_area_number, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).apply(white_line_count)
 
     return white_line.sum()
 
 
 def get_panje():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['1'].isna() | ~temp['5'].isna() | ~temp['4'].isna()]
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number].isin([1, 5, 4])]
 
     def white_line_count(temp_df):
-        one = temp_df["1"].values
-        five = temp_df["5"].values
+        one = temp_df.loc[temp_df[leg_area_number] == 1, finger_number].values
+        five = temp_df.loc[temp_df[leg_area_number] == 5, finger_number].values
         values = np.unique(np.concatenate([one, five]))
         values = values[~np.isnan(values)]
         if values.shape[0] != 0:
-            four = temp_df["4"].values
+            four = temp_df.loc[temp_df[leg_area_number] == 4, finger_number].values
             four = four[~np.isnan(four)]
             if four.shape[0] != 0:
                 return values.shape[0] - np.intersect1d(four, values).shape[0]
@@ -78,7 +81,7 @@ def get_panje():
         else:
             return 0
 
-    panje = temp[[cow_number_column, day_column, month_column, year_column, "1", "5", "4"]].groupby(
+    panje = temp[[cow_number_column, day_column, month_column, year_column, leg_area_number, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).apply(white_line_count)
     result = panje.sum()
 
@@ -86,127 +89,128 @@ def get_panje():
 
 
 def get_pashne():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['6'].isna()]
-    pashne = temp[[cow_number_column, day_column, month_column, year_column, "6"]].groupby(
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number] == 6]
+
+    pashne = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).nunique()
     return pashne.sum()[0]
 
 
 def get_tarak():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['7'].isna() | ~temp['8'].isna() | ~temp['11'].isna() | ~temp['12'].isna()]
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number].isin([7, 8, 11, 12])]
 
     def inner_tarak(temp_df):
-        seven = temp_df["7"].values
-        eight = temp_df["8"].values
-        eleven = temp_df["11"].values
-        twelve = temp_df["12"].values
+        seven = temp_df.loc[temp_df[leg_area_number] == 7, finger_number].values
+        eight = temp_df.loc[temp_df[leg_area_number] == 5, finger_number].values
+        eleven = temp_df.loc[temp_df[leg_area_number] == 11, finger_number].values
+        twelve = temp_df.loc[temp_df[leg_area_number] == 12, finger_number].values
         values = np.unique(np.concatenate([seven, eight, eleven, twelve]))
         values = values[~np.isnan(values)]
         return values.shape[0]
 
-    tarak = temp[[cow_number_column, day_column, month_column, year_column, "7", "8", "11", "12"]].groupby(
+    tarak = temp[[cow_number_column, day_column, month_column, year_column, leg_area_number, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).apply(inner_tarak)
     return tarak.sum()
 
 
 def get_nine():
-    temp = df[df[new_limp_column] == "*"]
-    temp = temp[~temp['9'].isna()]
-    kaf_som = temp[[cow_number_column, day_column, month_column, year_column, "9"]].groupby(
+    temp = df[df[new_limp_column]]
+    temp = temp[temp[leg_area_number] == 9]
+
+    kaf_som = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
         [cow_number_column, day_column, month_column, year_column]).nunique()
     return kaf_som.sum()[0]
 
 
 def get_visit():
-    temp = df[df['بازدید لنگش'] == "*"]
+    temp = df[df['reference_cause_limp_visit'] == 1]
     visit_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return visit_count.sum()[0]
 
 
 def get_new_limp():
-    temp = df[df[new_limp_column] == "*"]
+    temp = df[df[new_limp_column]]
     limp_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return limp_count.sum()[0]
 
 
 def get_sad():
-    temp = df[df["100 روزه"] == "*"]
+    temp = df[df["reference_cause_hundred_days"] == 1]
     sad_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return sad_count.sum()[0]
 
 
 def get_dry():
-    temp = df[df["خشکی"] == "*"]
+    temp = df[df["reference_cause_dryness"] == 1]
     dry_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return dry_count.sum()[0]
 
 
 def get_delay():
-    temp = df[df["عقب مانده تولید مثلی"] == "*"]
+    temp = df[df["reference_cause_lagged"] == 1]
     delay_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return delay_count.sum()[0]
 
 
 def get_group():
-    temp = df[df['سم چینی گروهی'] == "*"]
+    temp = df[df['reference_cause_group_hoof_trim'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_long():
-    temp = df[df['سم بلند'] == "*"]
+    temp = df[df['reference_cause_long_hoof'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_som_chini():
-    temp = df[df['سم چینی'] == "*"]
+    temp = df[df['other_info_hoof_trim'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_high_score():
-    temp = df[df['اسکور بالا'] == "*"]
+    temp = df[df['reference_cause_high_score'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_talise():
-    temp = df[df['تلیسه'] == "*"]
+    temp = df[df['reference_cause_heifer'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_erjaii():
-    temp = df[df['ارجاعی'] == "*"]
+    temp = df[df['reference_cause_referential'] == 1]
     group_count = temp[[cow_number_column, day_column, month_column, year_column]].groupby(
         [day_column, month_column, year_column]).nunique()
     return group_count.sum()[0]
 
 
 def get_takhte():
-    temp = df[df['تخته گذاری'] == "*"]
+    temp = df[df['other_info_boarding'] == 1]
 
     def inner_takhte(temp_df):
-        fingers = np.unique(temp_df[areas].values)
+        fingers = np.unique(temp_df[finger_number].values)
         fingers = fingers[~np.isnan(fingers)]
         return fingers.shape[0]
 
-    group_count = temp[[cow_number_column, day_column, month_column, year_column] + areas].groupby(
-        [cow_number_column, day_column, month_column, year_column]).apply(
-        inner_takhte)
+    group_count = temp[[cow_number_column, day_column, month_column, year_column, finger_number]].groupby(
+        [cow_number_column, day_column, month_column, year_column]).apply(inner_takhte)
     return group_count.sum()
 
 
